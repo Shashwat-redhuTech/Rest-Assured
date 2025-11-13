a8c3c9db5d54b243ca89112c0c3b16438969b88e
Shashwat <code@redhutechnology.in>
2025-11-13 16:15:49 +0530

    parsingresponseupdated

--- src/test/java/day4/ParsingJsonResponse.java
+++ src/test/java/day4/ParsingJsonResponse.java
@@ -9,39 +9,39 @@ import org.json.JSONObject;
 import org.testng.annotations.Test;
 public class ParsingJsonResponse {
 
-//    @Test
-//    void testJsonResponse1() {
-//        given()
-//            .contentType(ContentType.JSON)
-//        .when()
-//            .get("http://localhost:3000/book")
-//        .then()
-//            .statusCode(200)
-//            .body("[3].title", equalTo("Foundation"));
-//    } 
-//    
+   @Test
+   void testJsonResponse1() {
+       given()
+           .contentType(ContentType.JSON)
+       .when()
+           .get("http://localhost:3000/book")
+       .then()
+           .statusCode(200)
+           .body("[3].title", equalTo("Foundation"));
+   } 
+   
   //Approach2
-  @Test
-  void testJsonResponse2() {
-      // Step 1: Send GET request and capture response
-      Response res = given()
-          .contentType(ContentType.JSON)
-      .when()
-          .get("http://localhost:3000/book");
+//   @Test
+//   void testJsonResponse2() {
+//       // Step 1: Send GET request and capture response
+//       Response res = given()
+//           .contentType(ContentType.JSON)
+//       .when()
+//           .get("http://localhost:3000/book");
 
-      // Step 2: Convert response to String
-      String responseBody = res.asString();
+//       // Step 2: Convert response to String
+//       String responseBody = res.asString();
 
-      // Step 3: Since the root element is an array, parse it as JSONArray
-      JSONArray booksArray = new JSONArray(responseBody);
+//       // Step 3: Since the root element is an array, parse it as JSONArray
+//       JSONArray booksArray = new JSONArray(responseBody);
 
-      // Step 4: Iterate through each book and print the title
-      for (int i = 0; i < booksArray.length(); i++) {
-          JSONObject book = booksArray.getJSONObject(i);
-          String title = book.getString("title");
-          System.out.println(title);
-      }
-  }
+//       // Step 4: Iterate through each book and print the title
+//       for (int i = 0; i < booksArray.length(); i++) {
+//           JSONObject book = booksArray.getJSONObject(i);
+//           String title = book.getString("title");
+//           System.out.println(title);
+//       }
+//   }
 
 }
 
